9199272c840d727558dfe9eb03d5f43ab5523519
RyanYau <abbyryan414@gmail.com>
2021-10-16 00:03:55 +0800

    added subject chapter selection page

--- app/src/main/java/com/example/test1/MainActivity.kt
+++ app/src/main/java/com/example/test1/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.test1
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        findViewById<Button>(R.id.exercise_button).setOnClickListener {
+            val intent = Intent( this, exercise::class.java)
+            startActivity(intent)
+        }
+        findViewById<Button>(R.id.random_button).setOnClickListener {
+            val intent = Intent( this, leaderboard::class.java)
+            startActivity(intent)
+        }
+        findViewById<Button>(R.id.statistics_button).setOnClickListener {
+            val intent = Intent( this, statistics::class.java)
+            startActivity(intent)
+        }
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/test1/exercise.kt
@@ -0,0 +1,30 @@
+package com.example.test1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class exercise : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercise)
+        findViewById<Button>(R.id.phy_button).setOnClickListener {
+            val intent = Intent( this, phy_exercise::class.java)
+            startActivity(intent)
+        }
+        findViewById<Button>(R.id.chem_button).setOnClickListener {
+            val intent = Intent( this, chem_exercise::class.java)
+            startActivity(intent)
+        }
+        findViewById<Button>(R.id.math_button).setOnClickListener {
+            val intent = Intent( this, math_exercise::class.java)
+            startActivity(intent)
+        }
+        findViewById<Button>(R.id.bio_button).setOnClickListener {
+            val intent = Intent( this, bio_exercise::class.java)
+            startActivity(intent)
+        }
+    }
+
+}
