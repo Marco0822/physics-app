dcf73b488d912be5767dc2bedb288627b2a3427f
RyanYau <abbyryan414@gmail.com>
2021-12-02 12:46:26 +0800

    2/12 update

--- app/src/main/java/com/example/test1/load1.kt
+++ app/src/main/java/com/example/test1/load1.kt
@@ -3,6 +3,7 @@ package com.example.test1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import java.util.concurrent.TimeUnit
 
 class load1 : AppCompatActivity() {
@@ -10,26 +11,34 @@ class load1 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_load1)
         //val random_number = (1..4).random()
+
         val checker = intent.getStringExtra("page_id")
-        TimeUnit.SECONDS.sleep(1L)
-        if (checker == "exercise") {
-            val intent = Intent(this, exercise::class.java)
-            startActivity(intent)
-        }
-        if (checker == "statistics") {
-            val intent = Intent(this, statistics::class.java)
-            startActivity(intent)
+        //TimeUnit.SECONDS.sleep(1L)
+        val timer = object: CountDownTimer(2500, 100) {
+            override fun onTick(millisUntilFinished: Long){}
+            override fun onFinish() {
+                if (checker == "exercise") {
+                    val intent = Intent(this, exercise::class.java)
+                    startActivity(intent)
+                }
+                if (checker == "statistics") {
+                    val intent = Intent(this, statistics::class.java)
+                    startActivity(intent)
 
-        }
-        if (checker == "leaderboard") {
-            val intent = Intent(this, leaderboard::class.java)
-            startActivity(intent)
+                }
+                if (checker == "leaderboard") {
+                    val intent = Intent(this, leaderboard::class.java)
+                    startActivity(intent)
 
-        }
-        if (checker == "chat2") {
-            val intent = Intent(this, chat2::class.java)
-            startActivity(intent)
+                }
+                if (checker == "chat2") {
+                    val intent = Intent(this, chat2::class.java)
+                    startActivity(intent)
 
+                }
+            }
         }
+
+        timer.start()
     }
 }
