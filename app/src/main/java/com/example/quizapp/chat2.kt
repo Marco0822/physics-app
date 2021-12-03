08fa0d201ff0ba21061c4a304ccb8b43a7ae2393
SimplyComplexity <45217631+SimplyComplexity@users.noreply.github.com>
2021-12-03 17:39:37 +0800

    added everything

--- app/src/main/java/com/example/quizapp/chat2.kt
+++ app/src/main/java/com/example/quizapp/chat2.kt
@@ -2,10 +2,22 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class chat2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat2)
+
+        // get reference to button
+        val btn_click_me = findViewById(R.id.send) as Button
+        // set on-click listener
+        btn_click_me.setOnClickListener {
+            // your code to perform when the user clicks on the button
+            Toast.makeText(this ,"In development!", Toast.LENGTH_SHORT).show()
     }
+
+
 }
+    }
